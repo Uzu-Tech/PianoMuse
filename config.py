2ac61da230dffdb327aeb98c753dbc7f61a41445
Kobi Uzu <68594772+Uzu-Tech@users.noreply.github.com>
2024-12-21 01:00:44 +0400

    Add a config file

---
+++ config.py
@@ -0,0 +1,30 @@
+# config.py
+DATA_DIR = "data"
+ERROR_DIR = "pickle files/error.txt"
+SAVE_DIR = "pickle files/merged_tokens.pkl"
+MERGES_DIR = "pickle files/merges.pkl"
+GENRES = [
+    "Ambient",
+    "Blues",
+    "Children",
+    "Classical",
+    "Country",
+    "Electronic",
+    "Folk",
+    "Jazz",
+    "Latin",
+    "Pop",
+    "Rap",
+    "Reggae",
+    "Religious",
+    "Rock",
+    "Soul",
+    "Soundtracks",
+    "Unknown",
+    "World"
+]
+
+# Tokenizer settings
+VOCAB_SIZE = 2**16 # 65536
+
+# Hyperparameters
