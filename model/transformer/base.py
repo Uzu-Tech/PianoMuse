b7c1bffce2a6bc331df58cde565876203903f597
Kobi Uzu <68594772+Uzu-Tech@users.noreply.github.com>
2025-01-02 00:59:30 +0400

    Add one to vocab size because of pad token

--- model/transformer/base.py
+++ model/transformer/base.py
@@ -1,5 +1,4 @@
 import lightning as pL
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -28,7 +27,8 @@ class RelativeTransformerPredictor(pL.LightningModule):
 
     def _create_model(self):
         self._embeddings = nn.Embedding(
-            self.hparams.vocab_size,
+            # Account for padding token
+            self.hparams.vocab_size + 1,
             self.hparams.embed_dim,
             padding_idx=self.hparams.padding_idx,
         )
@@ -39,9 +39,9 @@ class RelativeTransformerPredictor(pL.LightningModule):
 
         self._multi_layered_decoder = MultiLayeredDecoder(
             self.hparams.num_layers,
-            self.hparams.context_size,
-            self.hparams.embed_dim,
-            self.hparams.num_heads,
+            context_size=self.hparams.context_size,
+            embed_dim=self.hparams.embed_dim,
+            num_attention_heads=self.hparams.num_heads,
             feed_forward_dim=self.hparams.embed_dim * 4,
             dropout=self.hparams.dropout,
         )
@@ -52,7 +52,7 @@ class RelativeTransformerPredictor(pL.LightningModule):
             nn.LayerNorm(self.hparams.embed_dim),
             nn.ReLU(inplace=True),
             nn.Dropout(self.hparams.dropout),
-            nn.Linear(self.hparams.embed_dim, self.hparams.vocab_size),
+            nn.Linear(self.hparams.embed_dim, self.hparams.vocab_size + 1),
         )
 
         self._loss = nn.CrossEntropyLoss(ignore_index=self.hparams.padding_idx)
@@ -76,7 +76,7 @@ class RelativeTransformerPredictor(pL.LightningModule):
         return x
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+        optimizer = optim.AdamW(self.parameters(), lr=self.hparams.learning_rate)
         # We don't return the lr scheduler since we need to apply it per iteration, not per epoch
         self.lr_scheduler = CosineWarmupScheduler(
             optimizer, warmup=self.hparams.warmup, max_iters=self.hparams.max_iters
@@ -92,10 +92,14 @@ class RelativeTransformerPredictor(pL.LightningModule):
         outputs = self.forward(input)
         # Combine batch and seq len dimensions when calculating loss
         loss = self._loss(outputs.view(-1, outputs.size(-1)), targets.view(-1))
-        #
         accuracy = (outputs.argmax(dim=-1) == targets).float().mean()
-        self.log(f"{log_mode}_loss", loss)
-        self.log(f"{log_mode}_accuracy", accuracy)
+        self.log_dict(
+            {
+                f"{log_mode}_loss": loss,
+                f"{log_mode}_accuracy": accuracy
+            },
+            on_epoch=True
+        )
         return loss, accuracy
     
     def training_step(self, batch, batch_idx):
@@ -107,3 +111,21 @@ class RelativeTransformerPredictor(pL.LightningModule):
 
     def test_step(self, batch, batch_idx):
         _ = self._get_loss(batch, log_mode="test")
+
+    def inference(self, x, temp=1):
+        logits = self.forward(x)  # Get raw logits from the model
+
+        # Adjust logits by temperature
+        logits = logits / temp
+
+        # Apply softmax to convert logits to probabilities
+        probs = torch.softmax(logits, dim=-1)
+
+        # Sample a single token for each row in the batch
+        predictions = torch.argmax(probs.view(-1, probs.size(-1)), dim=-1)
+
+        # Reshape predictions to match input's batch size and sequence length
+        #predictions = predictions.view(x.size(0), x.size(1))
+        print(predictions)
+
+        return predictions
